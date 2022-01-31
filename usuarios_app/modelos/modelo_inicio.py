6860dd7a20afaa00f7abc58fa998f16590118e3b
AMontoya224 <97045678+AMontoya224@users.noreply.github.com>
2022-01-31 15:50:06 -0500

    Core Flask + SQL

--- usuarios_app/modelos/modelo_inicio.py
+++ usuarios_app/modelos/modelo_inicio.py
@@ -3,6 +3,8 @@ from flask import flash
 from usuarios_app.config.mysqlconnection import connectToMySQL
 
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+PASS_REGEX = re.compile(r'^[a-zA-Z]+[0-9]+|[0-9]+[a-zA-Z]+$')
+NAME_REGEX = re.compile(r'^[a-zA-Z]+$')
 
 class Users:
     def __init__( self, id, first_name, last_name, email, password, gender, created_at, update_at ):
@@ -44,3 +46,45 @@ class Users:
         if len(resultado) < 1:
             return False
         return resultado[0]
+    
+    @classmethod
+    def verificarRegistro(cls, user):
+        is_valid = True
+        data = { 
+            "email" : user["email"] 
+            }
+        userInDashboard = Users.conseguirEmail(data)
+
+        if not NAME_REGEX.match(user["first_name"]): 
+            flash("El nombre solo puede contener letras", "register")
+            is_valid = False
+
+        if len(user["first_name"]) < 3:
+            flash("El nombre debe contener al menos 3 letras", "register")
+            is_valid = False
+
+        if not NAME_REGEX.match(user["last_name"]): 
+            flash("El apellido solo puede contener letras", "register")
+            is_valid = False
+        
+        if len(user["last_name"]) < 3:
+            flash("El apellido debe contener al menos 3 letras", "register")
+            is_valid = False
+
+        if userInDashboard:
+            flash("El e-mail ya está registrado, pruebe otro", "register")
+            is_valid = False
+
+        if not PASS_REGEX.match(user["password"]): 
+            flash("La contraseña debe contener al menos un número y una letra", "register")
+            is_valid = False
+        
+        if len(user["password"]) < 3:
+            flash("La contraseña debe contener al menos 8 caracteres", "register")
+            is_valid = False
+
+        if user["password"] != user["confirmarPassword"]:
+            flash("Las contraseñas son distintas", "register")
+            is_valid = False
+
+        return is_valid
